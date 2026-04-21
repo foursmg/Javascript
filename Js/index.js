84c094a190863715c6cde7d7b8c2f9ff5d8c9211
foursmg <141165954+foursmg@users.noreply.github.com>
2026-04-22 00:16:09 +0530

    ...

---
+++ Js/index.js
@@ -0,0 +1,15 @@
+console.time('Tracking Time') //here tracking time is an identifier
+console.log('Hello JavaScript');
+console.log("hi");
+console.log(1+2);
+console.log(3%2);
+console.log("Boolean "+ true);
+console.log([11,22,33,44,55]);
+console.log({Bike: "Honda", Model: 650});
+console.table({Bike: "Honda", Model: 650});
+console.warn("Warning!!!");
+console.timeEnd('Tracking Time');
+console.assert(24>18, 'Age > 18 Means Vote!!!'); //No error Nothing
+//console.assert(24<18,'Age>18'); //Error
+console.error("This is an Error");
+console.clear();
