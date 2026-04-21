b9ccdee172a151ebd6d9c85b22d44371cdd2bb99
foursmg <141165954+foursmg@users.noreply.github.com>
2026-04-22 00:22:20 +0530

    ...

--- Js/A_index.js
+++ Js/A_index.js
@@ -1,6 +1,6 @@
 console.time('Tracking Time') //here tracking time is an identifier
 console.log('Hello JavaScript');
-console.log("hi");
+console.log("hii");
 console.log(1+2);
 console.log(3%2);
 console.log("Boolean "+ true);
@@ -9,7 +9,13 @@ console.log({Bike: "Honda", Model: 650});
 console.table({Bike: "Honda", Model: 650});
 console.warn("Warning!!!");
 console.timeEnd('Tracking Time');
+
 console.assert(24>18, 'Age > 18 Means Vote!!!'); //No error Nothing
-//console.assert(24<18,'Age>18'); //Error
-console.error("This is an Error");
-console.clear();
+console.assert(24<18,'Age>18'); //Error
+// console.assert() 
+// If Condition true -> Nothing Happens
+// If Condition false -> It Prints an error message in the console
+// it does not stops execution or crash program -> Only Logs a message in console (for debugging)
+
+//console.error("This is an Error");
+//console.clear();
