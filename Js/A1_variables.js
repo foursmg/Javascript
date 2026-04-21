3b49fa68fb48a537989d191e820a62478f84aa9d
foursmg <141165954+foursmg@users.noreply.github.com>
2026-04-22 01:24:50 +0530

    ...

---
+++ Js/A1_variables.js
@@ -0,0 +1,38 @@
+//Varibales
+
+// 'var' and 'let' Keyword is used to declare variables
+
+var num;             //variable declared
+num=21;             //variable Initialize
+console.log(num);  //23
+
+var number = 231;
+console.log(number);   //231
+number = 75;
+console.log(number);   //75
+
+
+var name = "Kartik";
+console.log(name);
+
+var quote = 'Bhagat Singh - "Inquilab Zindabad"';
+console.log(quote);
+
+//Rules for Naming Varibale
+//Variable name must start with -> a letter, an underscore(_), or dollor sign($).
+//Variables are case Sensitive ->  Num and num Both are different variable
+//Variable cannot start with a number.
+//Varibale names cannot be keywords.
+
+
+//Constants
+//A constant is a type of variable whose value cannot be changed.
+//Always initialize a constant During Declaration
+const numValue = 76;
+console.log(numValue);
+numValue = 87;          //it will produce error.
+console.log(numValue);
+
+const movieName= "Kartik calling Kartik";
+console.log(movieName);
+confirm
