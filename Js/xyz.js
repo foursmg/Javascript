d71e1dda6969589ae94626ac1831c3309a390375
foursmg <141165954+foursmg@users.noreply.github.com>
2026-07-17 12:19:01 +0530

    ...

--- Js/xyz.js
+++ Js/xyz.js
@@ -1,6 +1,36 @@
-let arr = [1,2,3,4,5];
-let nums= [];
-for(let i=0;i<arr.length;i++){
-    nums[i] = arr[i]*arr[i];
-}
-console.log(nums);
+
+// Object example.
+
+let student = {
+    name: 'Kelly',
+    age: 32,
+    course: "MS",
+};
+
+console.log(student);
+
+
+// Converting object into string
+let jsonData = JSON.stringify(student);
+
+console.log(jsonData);
+
+// Storing the string on local storage
+localStorage.setItem("studentfirst",jsonData);
+
+let stud = localStorage.getItem("studentfirst");
+console.log(stud);
+
+let kelly = JSON.parse(stud);
+
+console.log(kelly);
+
+console.log("----------------------------------");
+
+
+
+localStorage.setItem("Pin",87638);
+
+let num = localStorage.getItem("Pin");
+console.log(num);
+console.log(typeof(num));
